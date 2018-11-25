7ead58502a08c389dbbcd84ba725ec77d43c279a
Kolten Sturgill <koltensturgill92@gmail.com>
2018-11-24 23:23:26 -0600

    modified makefile, added stack to loader, C file for interfacing with assembly

--- loader.s
+++ loader.s
@@ -4,6 +4,12 @@ MAGIC_NUMBER equ 0x1BADB002     ; DEFINE THE MAGIC NUMBER CONSTANT
 FLAGS        equ 0x0            ; multiboot flags
 CHECKSUM     equ -MAGIC_NUMBER  ; calc checksum
 
+KERNEL_STACK_SIZE equ 4096                  ; size of stack in bytes
+
+section .bss                                ; static variables 
+align 4                                     ; align at 4 bytes
+kernel_stack:                               ; label points to beginning of memory
+    resb KERNEL_STACK_SIZE                  ; reserve stack for the kernel
 
 section .text:                  ; start of the code section
 align 4
@@ -12,6 +18,8 @@ align 4
     dd CHECKSUM                 ; and the checksum
 
 loader:
+    mov esp, kernel_stack + KERNEL_STACK_SIZE   ; point esp to the start of the
+                                                ; stack (end of memory area)
     mov eax, 0xCAFEBABE         ; the loader label (entry point for linker script)
 
 .loop:
